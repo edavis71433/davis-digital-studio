c45ef36e6c2ea97b85ddb29d63b1884f284e9891
edavis71433 <eric@davisdigitalstudio.com>
2026-07-04 10:47:15 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DDS_CONFIG = {
+  SUPABASE_URL: "https://rlochuyqrpybhanaujmb.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsb2NodXlxcnB5YmhhbmF1am1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxODQ2MTYsImV4cCI6MjA5ODc2MDYxNn0.iK9OVceLAWw2lmgnQkhA-eGsk4Ck4pCgaRxXw0nWvfg",
+  STUDIO_NAME: "Hettie Orange Designs"
+};
